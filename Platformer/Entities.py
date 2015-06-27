9ca3f7711459338ffa46869850e988f8080ef952
AverageBrownie <legofan4444@gmail.com>
2015-06-27 03:49:31 -0400

    Movement is screwed had it then lost it

--- Platformer/Entities.py
+++ Platformer/Entities.py
@@ -13,13 +13,41 @@ class Player():
         self.movingLeft = False
         self.movingDown = False
         self.movingUp = False
+		
     def Update(self):
-	pygame.draw.rect(gb.screen, (gb.white), player_1)
+		pygame.draw.rect(gb.screen, (gb.white), player_1)
+		if self.movingRight == True:
+			self.rect.x += self.speed
+		if self.movingLeft == True:
+			self.rect.x -= self.speed
+		if self.movingDown == True:
+			self.rect.y += self.speed
+		if self.movingUp == True:
+			self.rect.y -= self.speed
+		
+			for event in pygame.event.get():
+				if event.type == pygame.KEYDOWN:
+					if event.key == pygame.K_w:
+						player_1.movingUp = True
+					if event.key == pygame.K_s:
+						player_1.movingDown = True
+					if event.key == pygame.K_a:
+						player_1.movingLeft = True
+					if event.key == pygame.K_d:
+						player_1.movingRight = True
+				if event.type == pygame.KEYUP:
+					if event.key == pygame.K_w:
+						player_1.movingUp = False
+					if event.key == pygame.K_s:
+						player_1.movingDown = False
+					if event.key == pygame.K_a:
+						player_1.movingLeft = False
+					if event.key == pygame.K_d:
+						player_1.movingRight = False
 
-        
- 
-		
-		
+
 player_1 = Player(100, 100)
+
+
 
 
